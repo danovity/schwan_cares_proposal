a23166212465c41656fc6bea47704e16aa5a24de
danovity <sean.xiaoxu.ye@gmail.com>
2018-10-13 09:36:59 -0400

    finished initial layout of the seasonal bundles section

--- src/Container/SeasonalBundles/SeasonalBundles.js
+++ src/Container/SeasonalBundles/SeasonalBundles.js
@@ -29,6 +29,58 @@ export default class SeasonalBundles extends Component {
             </div>
           </div>
         </div>
+        <div className="seasonal-bundles__container">
+          <figure className="seasonal-bundles__img-container">
+            <img
+              src="https://schwans-prod.imgix.net/images/modifiers/images/000/000/012/original/bundle_1.png?ch=Width%2CDPR%2CSave-Data&auto=format%2Ccompress&dpr=2&w=640"
+              alt="Premium Collection"
+              className="seasonal-bundles__img"
+            />
+          </figure>
+          <div className="seasonal-bundles__content">
+            <div className="seasonal-bundles__header">Premium Collection</div>
+            <div className="seasonal-bundles__description">
+              A wide variety of our finest seafood, poultry and meat.
+            </div>
+            <div className="seasonal-bundles__actions">
+              <div className="seasonal-bundles__price">$108.95</div>
+              <div className="seasonal-bundles__buttons">
+                <div className="button button__normal button__normal--shop">
+                  Shop
+                </div>
+                <div className="button button__normal button__normal--share">
+                  Share
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="seasonal-bundles__container">
+          <figure className="seasonal-bundles__img-container">
+            <img
+              src="https://schwans-prod.imgix.net/images/modifiers/images/000/000/012/original/bundle_1.png?ch=Width%2CDPR%2CSave-Data&auto=format%2Ccompress&dpr=2&w=640"
+              alt="Premium Collection"
+              className="seasonal-bundles__img"
+            />
+          </figure>
+          <div className="seasonal-bundles__content">
+            <div className="seasonal-bundles__header">Premium Collection</div>
+            <div className="seasonal-bundles__description">
+              A wide variety of our finest seafood, poultry and meat.
+            </div>
+            <div className="seasonal-bundles__actions">
+              <div className="seasonal-bundles__price">$108.95</div>
+              <div className="seasonal-bundles__buttons">
+                <div className="button button__normal button__normal--shop">
+                  Shop
+                </div>
+                <div className="button button__normal button__normal--share">
+                  Share
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     );
   }
