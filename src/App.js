49478e883015d29895ff54f4b2daa02f5ec0ca64
danovity <sean.xiaoxu.ye@gmail.com>
2018-10-13 21:59:10 -0400

    finished initial layout of the featured-products section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./Components/NavBar";
 import SectionHeader from "./Components/SectionHeader";
 import Banner from "./Container/Banner/Banner";
 import SeasonalBundles from "./Container/SeasonalBundles/SeasonalBundles";
+import FeaturedProducts from "./Container/FeaturedProducts/FeaturedProducts";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <SectionHeader />
         <SeasonalBundles />
         <SectionHeader />
+        <FeaturedProducts />
       </div>
     );
   }
---
+++ src/Container/FeaturedProducts/FeaturedProducts.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+
+export default class FeaturedProduct extends Component {
+  render() {
+    return (
+      <section className="featured-products">
+        <div className="featured-products__container">
+          <div className="featured-products__header">Meats</div>
+          <figure className="featured-products__image-container">
+            <img
+              src="https://schwans-prod.imgix.net/images/modifiers/images/000/000/003/original/SchwansFood_Meat1.jpg?ch=Width%2CDPR%2CSave-Data&auto=format%2Ccompress&dpr=2&w=200"
+              alt="Meats"
+              className="featured-products__image"
+            />
+          </figure>
+          <div className="featured-products__button">Earn 20% cash back</div>
+        </div>
+        <div className="featured-products__container">
+          <div className="featured-products__header">Meats</div>
+          <figure className="featured-products__image-container">
+            <img
+              src="https://schwans-prod.imgix.net/images/modifiers/images/000/000/003/original/SchwansFood_Meat1.jpg?ch=Width%2CDPR%2CSave-Data&auto=format%2Ccompress&dpr=2&w=200"
+              alt="Meats"
+              className="featured-products__image"
+            />
+          </figure>
+          <div className="featured-products__button">Earn 20% cash back</div>
+        </div>
+        <div className="featured-products__container">
+          <div className="featured-products__header">Meats</div>
+          <figure className="featured-products__image-container">
+            <img
+              src="https://schwans-prod.imgix.net/images/modifiers/images/000/000/003/original/SchwansFood_Meat1.jpg?ch=Width%2CDPR%2CSave-Data&auto=format%2Ccompress&dpr=2&w=200"
+              alt="Meats"
+              className="featured-products__image"
+            />
+          </figure>
+          <div className="featured-products__button">Earn 20% cash back</div>
+        </div>
+        <div className="featured-products__container">
+          <div className="featured-products__header">Meats</div>
+          <figure className="featured-products__image-container">
+            <img
+              src="https://schwans-prod.imgix.net/images/modifiers/images/000/000/003/original/SchwansFood_Meat1.jpg?ch=Width%2CDPR%2CSave-Data&auto=format%2Ccompress&dpr=2&w=200"
+              alt="Meats"
+              className="featured-products__image"
+            />
+          </figure>
+          <div className="featured-products__button">Earn 20% cash back</div>
+        </div>
+        <div className="featured-products__container">
+          <div className="featured-products__header">Meats</div>
+          <figure className="featured-products__image-container">
+            <img
+              src="https://schwans-prod.imgix.net/images/modifiers/images/000/000/003/original/SchwansFood_Meat1.jpg?ch=Width%2CDPR%2CSave-Data&auto=format%2Ccompress&dpr=2&w=200"
+              alt="Meats"
+              className="featured-products__image"
+            />
+          </figure>
+          <div className="featured-products__button">Earn 20% cash back</div>
+        </div>
+      </section>
+    );
+  }
+}
